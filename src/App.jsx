336942f87d7e32f59ecbfe43a63daa6a6b7fd3b2
Almaaz khan <khan.almaaz1055@gmail.com>
2025-03-13 16:39:22 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,55 @@ import React, { useEffect, useState } from "react";
 
 const App = () => {
   const cardArr = ["A", "C", "D", "A", "B", "B", "C", "D"];
-
+  const [flippedIndex, setFlippedIndex] = useState([]);
+  const [chances, setChances] = useState(0);
   const [cardObj, setCardObj] = useState(
     cardArr.map((card) => {
       return {
         name: card,
         isOpen: false,
-        id: null,
       };
     })
   );
-
+  let totalChances = 3;
   const flippedCard = cardObj.filter((card) => card.isOpen === true);
-  const sortedFlippedCard = flippedCard.sort((a, b) => a.id - b.id);
 
   const handleCardFlip = (index) => {
-    const duplicatedCardObjArr = [...cardObj];
-    duplicatedCardObjArr[index].isOpen = !duplicatedCardObjArr[index].isOpen;
-    duplicatedCardObjArr[index].id = Date.now();
-    setCardObj(duplicatedCardObjArr);
+    if (cardObj[index].isOpen || flippedIndex.length === 2) return;
+    if (chances < 3) {
+      setFlippedIndex([...flippedIndex, index]);
+      const duplicatedCardObjArr = [...cardObj];
+      duplicatedCardObjArr[index].isOpen = !duplicatedCardObjArr[index].isOpen;
+      setCardObj(duplicatedCardObjArr);
+    } else {
+      alert("Bs kr bhai haar gaya tu");
+    }
   };
 
-  let latestCard = null;
-  let beforeLatestCard = null;
-
   useEffect(() => {
-    if (sortedFlippedCard.length % 2 === 0) {
-      latestCard = sortedFlippedCard[sortedFlippedCard.length - 1];
-      beforeLatestCard = sortedFlippedCard[sortedFlippedCard.length - 2];
-      if (latestCard?.name !== beforeLatestCard?.name) {
-        latestCard.isOpen = false;
-        beforeLatestCard.isOpen = false;
+    if (flippedIndex.length === 2) {
+      const [firstIndex, secondIndex] = flippedIndex;
+      if (cardObj[firstIndex].name !== cardObj[secondIndex].name) {
+        setTimeout(() => {
+          if (chances < totalChances) {
+            const newCardObj = [...cardObj];
+            newCardObj[firstIndex].isOpen = false;
+            newCardObj[secondIndex].isOpen = false;
+            setCardObj(newCardObj);
+            setChances((prev) => (prev += 1));
+          }
+        }, 1000);
       }
+      setTimeout(() => setFlippedIndex([]), 800);
     }
   }, [flippedCard]);
 
+  useEffect(() => {
+    alert(`hi! you have ${totalChances} chances`);
+  }, []);
+
   return (
-    <div className="grid grid-cols-4 place-items-center gap-10 p-10 font-semibold">
+    <div className="grid max-sm:grid-cols-1 max-md:grid-cols-2 max-lg:grid-cols-3 grid-cols-4 place-items-center gap-10 p-10 font-semibold">
       {cardArr.map((card, index) => (
         <div
           className="border-2 border-black w-[200px] h-[300px] flex items-center justify-center cursor-pointer"
